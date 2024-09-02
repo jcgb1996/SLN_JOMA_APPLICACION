3242c7ce801e7d2857accf0bf510acfc3dc56e80
USER_Alan <USER@DESKTOP-C2L52RE>
2024-09-02 18:31:01 -0500

    Recreando el progreso anterior, una huevada oe no se guardo esa nota

---
+++ COM.JOMA.EMP.APLICACION/Dto/Request/Administracion/SucursalDto/SucursalReqDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM.JOMA.EMP.APLICACION.Dto.Request.Administracion.SucursalDto
+{
+    public class SucursalReqDto
+    {
+        public int Id { get; set; }
+
+
+        public string Nombre { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string CorreoElectronico { get; set; }
+
+        public string RUC { get; set; }
+
+        public string RepresentanteLegal { get; set; }
+
+        public string CedulaRepresentante { get; set; }
+
+        public string ActividadEconomica { get; set; }
+        public string HorarioAtencion { get; set; }
+    }
+}
---
+++ COM.JOMA.EMP.QUERY.SERVICE/Model/QRY_Sucursales.cs
@@ -0,0 +1,60 @@
+﻿using COM.JOMA.EMP.QUERY.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM.JOMA.EMP.QUERY.SERVICE.Model
+{
+    public partial class JomaQueryContext : JomaQueryContextEF
+    {
+        internal async Task<List<SucursalQueryDto>> GetSucursales(long IdCompania)
+        {
+
+
+
+            #region Descomentar
+            //var SP_NAME = "[QRY_Login]";
+            //List<LoginQueryDto>? Result = new();
+            //switch (QueryParameters.TipoORM)
+            //{
+            //    case JOMATipoORM.EntityFramework:
+            //        Result = LoginQueryDto?.FromSqlRaw($"[{SP_NAME}] @p0,@p1,@p2,@p3",
+            //            JOMAConversions.NothingToDBNULL(Usuario), JOMAConversions.NothingToDBNULL(ClaveEncriptada),
+            //            JOMAConversions.NothingToDBNULL(Compania), JOMAConversions.NothingToDBNULL(IPLogin)).ToList();
+            //
+            //        break;
+            //    case JOMATipoORM.Dapper:
+            //        using (var connection = jomaQueryContextDP.CreateConnection())
+            //        {
+            //            var parameters = new DynamicParameters();
+            //            parameters.Add("@LoginUsuario", JOMAConversions.NothingToDBNULL(Usuario), DbType.String);
+            //            parameters.Add("@ClaveUsuario", JOMAConversions.NothingToDBNULL(ClaveEncriptada), DbType.String);
+            //            parameters.Add("@RucCiaNube", JOMAConversions.NothingToDBNULL(Compania), DbType.String);
+            //            parameters.Add("@IpLogin", JOMAConversions.NothingToDBNULL(IPLogin), DbType.String);
+            //            Result = (await connection.QueryAsync<LoginQueryDto>(SP_NAME, parameters, commandType: CommandType.StoredProcedure)).ToList();
+            //        }
+            //        break;
+            //
+            //
+            //}
+            #endregion
+            var sucursales = new List<SucursalQueryDto>();
+            var tarea = Task.Run(() =>
+            {
+                sucursales = new List<SucursalQueryDto>
+                    {
+                        new SucursalQueryDto { Id = 8, Nombre = "Allan Stuart", AreaDesignada = "Oncología", Estado = 1,Maxrowcount=3 },
+                        new SucursalQueryDto { Id = 1, Nombre = "Smith White", AreaDesignada = "Neurología", Estado = 0,Maxrowcount=3 },
+                        new SucursalQueryDto { Id = 26, Nombre = "Gilbert Sandoval", AreaDesignada = "Cardiología", Estado = 1 ,Maxrowcount=3},
+                    };
+
+            });
+
+            await tarea;
+
+            return sucursales;
+        }
+    }
+}
