985b3b9d8be01d5bc9196eedc6755c16c135949d
USER_Alan <USER@DESKTOP-C2L52RE>
2024-09-04 14:31:59 -0500

    Cambio de prueba

--- COM.JOMA.EMP.QUERY/Dtos/SucursalQueryDto.cs
+++ COM.JOMA.EMP.QUERY/Dtos/SucursalQueryDto.cs
@@ -7,11 +7,6 @@ using System.Threading.Tasks;
 namespace COM.JOMA.EMP.QUERY.Dtos
 {
     public class SucursalQueryDto
-    {   
-        public long Id { get; set; }
-        public string Nombre { get; set; }
-        public string AreaDesignada { get; set; }
-        public int Estado { get; set; }
-        public int Maxrowcount { get; set; }
+    {
     }
 }
