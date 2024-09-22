e2306e0c371851dff52b1678adb16c5b5fe648e8
USER_Alan <USER@DESKTOP-C2L52RE>
2024-09-22 15:31:34 -0500

    continuan

--- COM.JOMA.EMP.QUERY.SERVICE/Model/QRY_GetSucursalesXIdEmpresa.cs
+++ COM.JOMA.EMP.QUERY.SERVICE/Model/QRY_GetSucursalesXIdEmpresa.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using COM.JOMA.EMP.DOMAIN.Constants;
+using COM.JOMA.EMP.DOMAIN.Tools;
+using COM.JOMA.EMP.QUERY.Dtos;
+using COM.JOMA.EMP.QUERY.Parameters;
+using Dapper;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace COM.JOMA.EMP.QUERY.SERVICE.Model
 {
-    internal class QRY_GetSucursalesXIdEmpresa
+    public partial class JomaQueryContext : JomaQueryContextEF
     {
+        internal async Task<List<SucursalQueryDto>> GetSucursalesXIdEmpresa(long IdCompania)
+        {
+            var SP_NAME = "[dbo].[QRY_GetSucursalesXIdEmpresa]";
+            List<SucursalQueryDto>? Result = new();
+            switch (QueryParameters.TipoORM)
+            {
+                case JOMATipoORM.EntityFramework:
+                    Result = sucursalQueryDto?.FromSqlRaw($"[{SP_NAME}] @p0",
+                        JOMAConversions.NothingToDBNULL(IdCompania)).ToList();
+
+                    break;
+                case JOMATipoORM.Dapper:
+                    using (var connection = jomaQueryContextDP.CreateConnection())
+                    {
+                        var parameters = new DynamicParameters();
+                        parameters.Add("@IdEmpresa", JOMAConversions.NothingToDBNULL(IdCompania), DbType.Int64);
+                        Result = (await connection.QueryAsync<SucursalQueryDto>(SP_NAME, parameters, commandType: CommandType.StoredProcedure)).ToList();
+                    }
+                    break;
+
+
+            }
+            return Result;
+        }
     }
 }
