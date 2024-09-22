ccf8b8745974f1d3387c2a3cdf37e495eb1b61f3
USER_Alan <USER@DESKTOP-C2L52RE>
2024-09-22 15:31:12 -0500

    continuan

---
+++ COM.JOMA.EMP.QUERY.SERVICE/Model/QRY_GetSucursalesXIdEmpresa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM.JOMA.EMP.QUERY.SERVICE.Model
+{
+    internal class QRY_GetSucursalesXIdEmpresa
+    {
+    }
+}
